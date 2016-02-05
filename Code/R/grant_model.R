c6a6da36aa5a21a832e182e18fe65fc9f7cdb03a
Mhopf <maximilianhopf@gmail.com>
2016-02-05 12:56:06 +0100

    Added AUC and ROC Curve

--- Code/R/grant_model.R
+++ Code/R/grant_model.R
@@ -19,24 +19,28 @@ test <- filter(data_all, Set_ID == "Testing")
 validation <- filter(data_all, Set_ID == "Validation")
 
 
-# Dividing Sponsor.Code into Groups
+### Dividing Sponsor.Code into Groups, trainined by Traning Data, but applied to all data
 SC.Status <- with(train, table(Sponsor.Code, Grant.Status))
 SC.Status <- cbind(SC.Status, SC.Status[,2]/rowSums(SC.Status))
 SC.Status[,3][is.na(SC.Status[,3])] <- 0
-SC.Status <- cbind(SC.Status, cut(SC.Status[,3], 10))
-
-# TODO: Map Trained Sponsor Groups to whole dataset
-#train[Sponsor.Group] <- 
+tmp <- cbind(row.names(SC.Status), cut(SC.Status[,3], 10))
+# Add new Column to all datasets
+train$SC.Group <- as.numeric(tmp[,2][match(as.character(train$Sponsor.Code), tmp)])
+test$SC.Group <- as.numeric(tmp[,2][match(as.character(test$Sponsor.Code), tmp)])
+validation$SC.Group <- as.numeric(tmp[,2][match(as.character(validation$Sponsor.Code), tmp)])
 
 
 
 
 ## Simple Random Forest Model
 # Sponsor.Code has too many fators
-train.rf <- select(train, Grant.Status, Grant.Category.Code, Contract.Value.Band, starts_with("Dep."), starts_with("Seob."),
-                   A..papers, A.papers, B.papers, C.papers, Dif.countries, Number.people, PHD, Max.years.univ, Grants.succ,
-                   Grants.unsucc, Departments, Perc_non_australian)
-rf <- randomForest(Grant.Status~., data=train.rf, ntree=1000)
+variables <- colnames(select(train, Grant.Status, Grant.Category.Code, Contract.Value.Band, starts_with("Dep."), starts_with("Seob."),
+               A..papers, A.papers, B.papers, C.papers, Dif.countries, Number.people, PHD, Max.years.univ, Grants.succ,
+               Grants.unsucc, Departments, Perc_non_australian, Season, SC.Group, Weekday, Month, Day.of.Month))
+
+
+train.rf <- select(train, .dots = variables)
+rf <- randomForest(Grant.Status~., data=train.rf, ntree=1500)
 pred_rf <- predict(rf, test)
 t_rf <- table(test$Grant.Status, pred_rf)
 acc <- (t_rf[1,1] + t_rf[2,2])/sum(t_rf)
@@ -44,5 +48,4 @@ acc
 
 
 
-
-
+save(rf.77, file=".//..//..//Data//RData//rf.77.RData")
