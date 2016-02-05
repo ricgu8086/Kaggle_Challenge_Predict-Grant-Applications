e88f2f7a84414e4f8483ed2fb5287cb597a76af4
Chris <iamcalledchris@hotmail.com>
2016-02-05 12:40:19 +0100

    one more

--- Code/R/csvpeopleBuilder.R
+++ Code/R/csvpeopleBuilder.R
@@ -1,13 +1,5 @@
 load('./Data/RData/peopleCoefs.RData')
 
-str(peopleCoefList)
-
-peopleCoefList
-
-
-attr(peopleCoefList, 'names')
-
-forRicardocsv = data.frame( factorName = attr(peopleCoefList, 'names'), coefficents = peopleCoefList )
 
 
 forRicardocsv = data.frame( factorName = attr(peopleCoefList, 'names'), coefficents = peopleCoefList )
